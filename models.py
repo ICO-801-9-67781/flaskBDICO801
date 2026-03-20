abd2e1a82d23efa2bb17c0620a20cff52b8ec867
Aldo Amaro <67781leo@universidaddeleon.edu.mx>
2026-03-19 23:46:53 -0600

    Migración 19/03/2026

--- models.py
+++ models.py
@@ -3,30 +3,38 @@ import datetime
 
 db = SQLAlchemy()
 
+
 class Alumnos(db.Model):
     __tablename__ = "alumnos"
-    matricula = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(250), nullable=False)
     apaterno = db.Column(db.String(50), nullable=False)
     amaterno = db.Column(db.String(150), nullable=False)
     edad = db.Column(db.Integer, nullable=False)
     correo = db.Column(db.String(100), nullable=False)
+    created_date = db.Column(db.DateTime, default=datetime.datetime.now)
 
-    def __init__(self, matricula, nombre, apaterno, amaterno, edad, correo):
-        self.matricula = matricula
+    cursos = db.relationship(
+        "Curso", secondary="inscripciones", back_populates="alumnos"
+    )
+
+    def __init__(self, nombre, apaterno, amaterno, edad, correo):
         self.nombre = nombre
         self.apaterno = apaterno
         self.amaterno = amaterno
         self.edad = edad
         self.correo = correo
 
+
 class Maestros(db.Model):
-    __tablename__ = 'maestros'
+    __tablename__ = "maestros"
     matricula = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(50))
     apellidos = db.Column(db.String(50))
     especialidad = db.Column(db.String(50))
     email = db.Column(db.String(50))
+
+    cursos = db.relationship("Curso", back_populates="maestro")
 
     def __init__(self, matricula, nombre, apellidos, especialidad, email):
         self.matricula = matricula
@@ -34,3 +42,44 @@ class Maestros(db.Model):
         self.apellidos = apellidos
         self.especialidad = especialidad
         self.email = email
+
+
+class Curso(db.Model):
+    __tablename__ = "cursos"
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    descripcion = db.Column(db.Text)
+
+    maestro_id = db.Column(
+        db.Integer, db.ForeignKey("maestros.matricula"), nullable=False
+    )
+
+    maestro = db.relationship("Maestros", back_populates="cursos")
+
+    alumnos = db.relationship(
+        "Alumnos", secondary="inscripciones", back_populates="cursos"
+    )
+
+    def __init__(self, nombre, descripcion, maestro_id):
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.maestro_id = maestro_id
+
+
+class Inscripcion(db.Model):
+    __tablename__ = "inscripciones"
+    id = db.Column(db.Integer, primary_key=True)
+    alumno_id = db.Column(db.Integer, db.ForeignKey("alumnos.id"), nullable=False)
+    curso_id = db.Column(db.Integer, db.ForeignKey("cursos.id"), nullable=False)
+    fecha_inscripcion = db.Column(db.DateTime, server_default=db.func.now())
+
+    __table_args__ = (
+        db.UniqueConstraint("alumno_id", "curso_id", name="uq_alumno_curso"),
+    )
+
+    alumno = db.relationship("Alumnos")
+    curso = db.relationship("Curso")
+
+    def __init__(self, alumno_id, curso_id):
+        self.alumno_id = alumno_id
+        self.curso_id = curso_id
